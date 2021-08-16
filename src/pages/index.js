5b1bb7fe5703752cad81d07ef4355bd9712140ed
Warren Behr <wbehr@users.noreply.github.com>
2021-08-16 14:53:39 -0400

    add links between pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function IndexPage() {
   return (
     <main>
       <h1>Hello Everybody!</h1>
+      <Link to="/about">About This Site</Link>
     </main>
   );
 }
