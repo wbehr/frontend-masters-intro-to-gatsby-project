f275c7d907662d9c829b8ad14fc70bb4815bcfa2
Warren Behr <wbehr@users.noreply.github.com>
2021-08-18 13:50:19 -0400

    add metadata, add SEO component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
     siteUrl: 'https://www.yourdomain.tld',
-    title: 'Frontend Masters Intro to Gatsby YAY',
+    title: 'Frontend Masters Intro to Gatsby',
     description: 'Frontend Masters Intro to Gatsby course projects',
     image:
       'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
   },
+  plugins: ['gatsby-plugin-react-helmet'],
 };
